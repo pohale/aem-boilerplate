1911a1d9ae9d27ee7cae1909bca88e0fab457ac0
Purva Ohale <pohale@adobe.com>
2025-10-01 15:01:53 +0530

    test

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,3 +1,10 @@
-// export default function decorate(block) {
-
-// }
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.classList.add('banner-back');
+    [...row.children].forEach((col, c) => {
+      if (c === 1) {
+        col.classList.add('slide-text');
+      }
+    });
+  });
+}
