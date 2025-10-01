409e85d4171ca3eda50993c15ee3b78101c5230d
Purva Ohale <pohale@adobe.com>
2025-10-01 14:32:39 +0530

    preetier fixes

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,37 +1,29 @@
-import { fetchPlaceholders, getMetadata } from "../../scripts/aem.js";
-
-const placeholders = await fetchPlaceholders(getMetadata("locale"));
-
-const { btnNxt, btnPre } = placeholders;
 export default function decorate(block) {
-  console.log("placeholders ---> ", placeholders, btnNxt, btnPre);
   const rows = [...block.children];
   [...block.children].forEach((row, r) => {
     if (r === 0) {
-      const nextbtn = document.createElement("button");
-      nextbtn.classList.add("btn");
-      nextbtn.classList.add("btn-next");
-      const node = document.createTextNode(btnNxt);
-      nextbtn.append(node);
+      const nextbtn = document.createElement('button');
+      nextbtn.classList.add('btn');
+      nextbtn.classList.add('btn-next');
+      nextbtn.innerHTML = row.innerHTML.trim();
       row.replaceWith(nextbtn);
     } else if (r === rows.length - 1) {
-      const prebtn = document.createElement("button");
-      prebtn.classList.add("btn");
-      prebtn.classList.add("btn-prev");
-      const node = document.createTextNode(btnPre);
-      prebtn.append(node);
+      const prebtn = document.createElement('button');
+      prebtn.classList.add('btn');
+      prebtn.classList.add('btn-prev');
+      prebtn.innerHTML = row.innerHTML.trim();
       row.replaceWith(prebtn);
     } else {
-      row.classList.add("slide");
+      row.classList.add('slide');
       [...row.children].forEach((col, c) => {
         if (c === 1) {
-          col.classList.add("slide-text");
+          col.classList.add('slide-text');
         }
       });
     }
   });
 
-  const slides = document.querySelectorAll(".slide");
+  const slides = document.querySelectorAll('.slide');
 
   // loop through slides and set each slides translateX
   slides.forEach((slide, indx) => {
@@ -39,7 +31,7 @@ export default function decorate(block) {
   });
 
   // select next slide button
-  const nextSlide = document.querySelector(".btn-next");
+  const nextSlide = document.querySelector('.btn-next');
 
   // current slide counter
   let curSlide = 0;
@@ -47,7 +39,7 @@ export default function decorate(block) {
   const maxSlide = slides.length - 1;
 
   // add event listener and navigation functionality
-  nextSlide.addEventListener("click", () => {
+  nextSlide.addEventListener('click', () => {
     // check if current slide is the last and reset current slide
     if (curSlide === maxSlide) {
       curSlide = 0;
@@ -62,10 +54,10 @@ export default function decorate(block) {
   });
 
   // select next slide button
-  const prevSlide = document.querySelector(".btn-prev");
+  const prevSlide = document.querySelector('.btn-prev');
 
   // add event listener and navigation functionality
-  prevSlide.addEventListener("click", () => {
+  prevSlide.addEventListener('click', () => {
     // check if current slide is the first and reset current slide to last
     if (curSlide === 0) {
       curSlide = maxSlide;
